31eb4b49d50d9302ed54be11ef9247d2c806d023
Omar Guevara <omar.guevara@bitsamericas.com>
2021-04-13 23:07:13 -0500

    read apis from spotify

--- web/modules/custom/spotify/src/Controller/SpotifyController.php
+++ web/modules/custom/spotify/src/Controller/SpotifyController.php
@@ -1,8 +1,16 @@
 <?php
-
+//https://developer.spotify.com/console/
+//https://developer.spotify.com/dashboard/applications
 namespace Drupal\spotify\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\Component\Utility\Html ;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use GuzzleHttp\Client;
+use Drupal\Component\Serialization\Json;
 
 /**
  * Returns responses for spotify routes.
@@ -12,14 +20,116 @@ class SpotifyController extends ControllerBase {
   /**
    * Builds the response.
    */
-  public function build() {
-
-    $build['content'] = [
-      '#type' => 'item',
-      '#markup' => $this->t('It works!'),
+  public function launch() {
+    $token = $this->getToken()['access_token'];
+    $endpoint_launch = $this->getConfig('spotify_endpoint_launch');
+    $method = 'GET';
+    $arg = [
+      'query' => [
+        'limit' => 10,            
+      ],
+      'headers' => [
+        'Accept' => 'application/json',
+        'Content-Type' => 'application/json',
+        'Authorization' =>  'Bearer '.$token
+      ],
     ];
 
-    return $build;
+    $launch = $this->send($endpoint_launch, $method, $arg);
+
+    return [
+      '#theme' => 'launch',
+      '#launch' => $launch,
+    ];
+  }
+
+   /**
+   * Builds the response.
+   */
+  public function artist($id) {
+    $token = $this->getToken()['access_token'];
+    $endpoint_artist = $this->getConfig('spotify_endpoint_artist').$id;
+    $method = 'GET';
+    $arg = [
+      'headers' => [
+        'Accept' => 'application/json',
+        'Content-Type' => 'application/json',
+        'Authorization' =>  'Bearer '.$token
+      ],
+    ];
+
+    $artist = $this->send($endpoint_artist, $method, $arg);
+    $tracks = $this->tracks($id);
+    
+    return [
+      '#theme' => 'artist',
+      '#artist' => $artist,
+      '#tracks' => $tracks
+    ]; 
+  }
+
+  /**
+   * Builds the response.
+   */
+  public function tracks($id) {
+    $token = $this->getToken()['access_token'];
+    $endpoint_artist = $this->getConfig('spotify_endpoint_artist').$id.'/top-tracks';
+    $method = 'GET';
+    $arg = [            
+      'query' => [
+        'country'=> 'CO',        
+      ],
+      'headers' => [
+        'Accept' => 'application/json',
+        'Content-Type' => 'application/json',
+        'Authorization' =>  'Bearer '.$token
+      ],
+    ];
+    $tracks = $this->send($endpoint_artist, $method, $arg);
+
+    return $tracks;
+  }
+
+  /**
+   * {@inheritdoc}
+   */   
+  public function getToken(){
+    $client_id = $this->getConfig('spotify_client_id'); 
+    $client_secret = $this->getConfig('spotify_client_secret'); 
+    $endpoint_token = $this->getConfig('spotify_endpoint_token');
+    $method = 'POST';
+
+    $arg = [
+      'form_params' => [
+        'grant_type'=> 'client_credentials',
+        'client_id' => $client_id,
+        'client_secret' => $client_secret,
+        'header' => array('Authorization: Basic '.base64_encode($client_id.':'.$client_secret))
+      ]
+    ];
+
+    return $this->send($endpoint_token, $method, $arg);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function send($end_point, $method, $arg){
+    $request = \Drupal::service('http_client_factory');
+    $api_client = $request->fromOptions(['verify' => FALSE]);
+    $pay_load = $api_client->request($method, $end_point, $arg);
+    $code = $pay_load->getStatusCode();
+    if ($code == 200) {
+      $body = $pay_load->getBody()->getContents();
+      return Json::decode($body);
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getConfig($data) {
+    return \Drupal::config('spotify.settings')->get($data);         
   }
 
 }
--- web/modules/custom/spotify/src/Form/SettingsForm.php
+++ web/modules/custom/spotify/src/Form/SettingsForm.php
@@ -28,32 +28,58 @@ class SettingsForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $form['example'] = [
+    $form['spotify_client_id'] = array(
       '#type' => 'textfield',
-      '#title' => $this->t('Example'),
-      '#default_value' => $this->config('spotify.settings')->get('example'),
-    ];
+      '#title' => $this->t('Client id Spotify'),
+      '#default_value' => $this->config('spotify.settings')->get('spotify_client_id'),
+      '#required' => TRUE,
+    );
+
+    $form['spotify_client_secret'] = array(
+      '#type' => 'textfield',
+      '#title' => $this->t('Client secret Spotify'),
+      '#default_value' => $this->config('spotify.settings')->get('spotify_client_secret'),
+      '#required' => TRUE,
+    );
+
+    $form['spotify_endpoint_token'] = array(
+      '#type' => 'textfield',
+      '#title' => $this->t('Endpoint for request authorization'),
+      '#maxlength' => 255,
+      '#default_value' => $this->config('spotify.settings')->get('spotify_endpoint_token'),
+      '#required' => TRUE,
+    );
+
+    $form['spotify_endpoint_launch'] = array(
+      '#type' => 'textfield',
+      '#title' => $this->t('Endpoint API Get an Launch'),
+      '#maxlength' => 255,
+      '#default_value' => $this->config('spotify.settings')->get('spotify_endpoint_launch'),
+      '#required' => TRUE,
+    );
+
+    $form['spotify_endpoint_artist'] = array(
+      '#type' => 'textfield',
+      '#title' => $this->t('Endpoint API Get an Artist'),
+      '#maxlength' => 255,
+      '#default_value' => $this->config('spotify.settings')->get('spotify_endpoint_artist'),
+      '#required' => TRUE,
+    );
+
     return parent::buildForm($form, $form_state);
   }
 
   /**
    * {@inheritdoc}
    */
-  public function validateForm(array &$form, FormStateInterface $form_state) {
-    if ($form_state->getValue('example') != 'example') {
-      $form_state->setErrorByName('example', $this->t('The value is not correct.'));
-    }
-    parent::validateForm($form, $form_state);
-  }
-
-  /**
-   * {@inheritdoc}
-   */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    $this->config('spotify.settings')
-      ->set('example', $form_state->getValue('example'))
-      ->save();
+    $config = $this->config('spotify.settings');
+
+    $config->set('spotify_client_id', $form_state->getValue('spotify_client_id'))->save();
+    $config->set('spotify_client_secret', $form_state->getValue('spotify_client_secret'))->save();
+    $config->set('spotify_endpoint_token', $form_state->getValue('spotify_endpoint_token'))->save();
+    $config->set('spotify_endpoint_launch', $form_state->getValue('spotify_endpoint_launch'))->save();
+    $config->set('spotify_endpoint_artist', $form_state->getValue('spotify_endpoint_artist'))->save();
     parent::submitForm($form, $form_state);
   }
-
 }
